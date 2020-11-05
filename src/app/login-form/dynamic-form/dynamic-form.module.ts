7dd8e0570a5fa2239f93a235392685ec1593e700
nbg <Gokul.Nb@rsa.com>
2020-11-05 09:42:43 +0530

    change directory

--- src/app/login-form/dynamic-form/dynamic-form.module.ts
+++ src/app/login-form/dynamic-form/dynamic-form.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
 import {DynamicFormComponent} from './containers/dynamic-form/dynamic-form.component';
-import { FormInputComponent } from './containers/dynamic-form/components/form-input/form-input.component'
+import { FormInputComponent } from '../dynamic-form/components/form-input/form-input.component';
 @NgModule({
-  declarations: [DynamicFormComponent,FormInputComponent ],
+  declarations: [DynamicFormComponent, FormInputComponent ],
   imports: [
     CommonModule, ReactiveFormsModule
   ],
